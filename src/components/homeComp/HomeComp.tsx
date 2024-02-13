baea87ca6f7be32595bf11eba92188d0d297d0bd
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-02-12 23:34:34 -0300

    style

--- src/components/homeComp/HomeComp.tsx
+++ src/components/homeComp/HomeComp.tsx
@@ -1,37 +1,32 @@
-import styles from './_HomeComp.module.scss';
-import img01 from '../../assets/Images/Slider/12.jpg'
-import img02 from '../../assets/Images/Slider/2.jfif'
-import img03 from '../../assets/Images/Slider/3.jfif'
-import img04 from '../../assets/Images/Slider/4.jfif'
-import img05 from '../../assets/Images/Slider/5.jfif'
-import img06 from '../../assets/Images/Slider/6.jfif'
-import img07 from '../../assets/Images/Slider/7.jfif'
-import img08 from '../../assets/Images/Slider/8.jfif'
-import img09 from '../../assets/Images/Slider/9.jfif'
-import img10 from '../../assets/Images/Slider/10.jfif'
-import img11 from '../../assets/Images/Slider/11.jfif'
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
+import styles from './_HomeComp.module.scss';
 
+// Importa tus imágenes
+import img01 from '../../assets/Images/Slider/12.jpg';
+import img02 from '../../assets/Images/Slider/2.jfif';
+import img03 from '../../assets/Images/Slider/3.jfif';
+import img04 from '../../assets/Images/Slider/4.jfif';
+import img05 from '../../assets/Images/Slider/5.jfif';
+import img06 from '../../assets/Images/Slider/6.jfif';
+import img07 from '../../assets/Images/Slider/7.jfif';
+import img08 from '../../assets/Images/Slider/8.jfif';
+import img09 from '../../assets/Images/Slider/9.jfif';
+import img10 from '../../assets/Images/Slider/10.jfif';
+import img11 from '../../assets/Images/Slider/11.jfif';
 
 const HomeComp = () => {
-
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Cambia a la siguiente imagen cada 3 segundos (ajusta según tus necesidades)
       setCurrentSlide((prevSlide) => (prevSlide + 1) % 11);
-    }, 3000); // Intervalo de 3 segundos
+    }, 3000); // Cambia a la siguiente imagen cada 3 segundos
+
     return () => clearInterval(interval);
   }, []); // Se ejecuta solo una vez al montar el componente
 
-  console.log(currentSlide)
-
-  const userAuthCompleteReducer = useSelector((state: any) => state.userAuth);
-  console.log('userAuthCompleteReducer.data :', userAuthCompleteReducer.data);
-  console.log('company');
-
+  // Define un arreglo con las imágenes
   const images = [
     img01,
     img02,
@@ -50,17 +45,22 @@ const HomeComp = () => {
     <div className={styles.container}>
       <div className={styles.sliderContainer}>
         <div className={styles.slider}>
-          <div className={styles.slide}>
-            <img
-              className={styles.img}
-              src={images[currentSlide]}
-              alt=""
-            />
-          </div>
+          {images.map((image, index) => (
+            <div
+              className={`${styles.slide} ${index === currentSlide ? styles.active : ''}`}
+              key={index}
+            >
+              <img
+                src={image}
+                alt=""
+                className={styles.img}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default HomeComp
+export default HomeComp;
