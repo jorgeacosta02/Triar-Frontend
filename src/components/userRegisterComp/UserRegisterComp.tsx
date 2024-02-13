b5c8e56be3a451bb4d07aad1a21e9037f8eab56c
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-02-13 17:55:14 -0300

    styles

--- src/components/userRegisterComp/UserRegisterComp.tsx
+++ src/components/userRegisterComp/UserRegisterComp.tsx
@@ -1,4 +1,4 @@
-import styles from '../userLoginComp/_UserLoginComp.module.scss';
+import styles from './_UserRegisterComp.module.scss';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { userRegisterSchema } from '../../validations/zodUserSchemas';
@@ -44,88 +44,90 @@ const UserRegisterComp
 
   return (
     <div className={styles.container}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h1 className={styles.title}>
-          Registrarse
-        </h1>
-        <div className={styles.inputBlock}>
-          <label htmlFor='firstName'>Nombre</label>
-          <input {...register('firstName')}
-              placeholder='Ingrese nombre...'
-          />
-          {errors.firstName && <span>{errors.firstName.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='lastName'>Apellido</label>
-          <input {...register(
-              'lastName',
-              { required: 'El apellido es requerido' })}
-              placeholder='Ingrese apellido...'
-          />
-          {errors.lastName && <span>{errors.lastName.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='dni'>Dni</label>
-          <input type='text' {...register(
-            'dni',
-            { required: 'El dni es requerido' })}
-            placeholder='Ingrese dni...'
-          />
-          {errors.dni && <span>{errors.dni.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='healthPlan'>Obra social</label>
-          <input {...register(
-              'healthPlan',
-            )}
-              placeholder='Ingrese su obra social...'
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='phone'>Teléfono</label>
-          <input {...register(
-              'phone',
-              { required: 'El número de teléfono es requerido' })}
-              placeholder='Ingrese número de teléfono...'
-          />
-          {errors.phone && <span>{errors.phone.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='email'>Correo</label>
-          <input {...register(
-            'email',
-            { required: 'El email es requerido' })}
-            placeholder='Ingrese correo...'
-          />
-        {errors.email && <span>{errors.email.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='password'>Contraseña</label>
-          <input type='password' {...register(
-            'password',
-            { required: 'La contraseña es requerida' })}
-            placeholder='Ingrese contraseña...'
-          />
-        {errors.password && <span>{errors.password.message}</span>}
-        </div>
-        <button
-          className={styles.submit}
-          type='submit'
+      <div className={styles.formContainer}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Enviar formulario
-        </button>
-      </form>
-      <p className={styles.link}>
-        Ya tenés una cuenta?
-        <Link 
-          to='/user-login'
-          className={styles.register}
-        >
-          Ingresar
-        </Link>
-      </p>
+          <h1 className={styles.title}>
+            Registrarse
+          </h1>
+          <div className={styles.inputBlock}>
+            <label htmlFor='firstName'>Nombre</label>
+            <input {...register('firstName')}
+                placeholder='Ingrese nombre...'
+            />
+            {errors.firstName && <span>{errors.firstName.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='lastName'>Apellido</label>
+            <input {...register(
+                'lastName',
+                { required: 'El apellido es requerido' })}
+                placeholder='Ingrese apellido...'
+            />
+            {errors.lastName && <span>{errors.lastName.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='dni'>Dni</label>
+            <input type='text' {...register(
+              'dni',
+              { required: 'El dni es requerido' })}
+              placeholder='Ingrese dni...'
+            />
+            {errors.dni && <span>{errors.dni.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='healthPlan'>Obra social</label>
+            <input {...register(
+                'healthPlan',
+              )}
+                placeholder='Ingrese su obra social...'
+            />
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='phone'>Teléfono</label>
+            <input {...register(
+                'phone',
+                { required: 'El número de teléfono es requerido' })}
+                placeholder='Ingrese número de teléfono...'
+            />
+            {errors.phone && <span>{errors.phone.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='email'>Correo</label>
+            <input {...register(
+              'email',
+              { required: 'El email es requerido' })}
+              placeholder='Ingrese correo...'
+            />
+          {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' {...register(
+              'password',
+              { required: 'La contraseña es requerida' })}
+              placeholder='Ingrese contraseña...'
+            />
+          {errors.password && <span>{errors.password.message}</span>}
+          </div>
+          <button
+            className={styles.submit}
+            type='submit'
+          >
+            Enviar formulario
+          </button>
+        </form>
+        <p className={styles.linkContainer}>
+          Ya tenés una cuenta?
+          <Link 
+            to='/user-login'
+            className={styles.login}
+          >
+            Ingresar
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
