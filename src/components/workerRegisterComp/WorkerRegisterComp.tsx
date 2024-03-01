2619fdae9875bb4f5f377e4c68f079c00964d289
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-01 17:45:18 -0300

    workerRegisterComp styles

--- src/components/workerRegisterComp/WorkerRegisterComp.tsx
+++ src/components/workerRegisterComp/WorkerRegisterComp.tsx
@@ -20,7 +20,6 @@ const WorkerRegisterComp
   });
 
 
-  // const API = 'http://localhost:4000'
 
   const onSubmit: SubmitHandler<IWorkerRegisterData> = async (data) => {
     console.log(data);
@@ -43,79 +42,81 @@ const WorkerRegisterComp
 
   return (
     <div className={styles.container}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h1 className={styles.title}>
-          Registrarse
-        </h1>
-        <div className={styles.inputBlock}>
-          <label htmlFor='firstName'>Nombre</label>
-          <input {...register('firstName')}
-              placeholder='Ingrese nombre...'
-          />
-          {errors.firstName && <span>{errors.firstName.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='lastName'>Apellido</label>
-          <input {...register(
-              'lastName',
-              { required: 'El apellido es requerido' })}
-              placeholder='Ingrese apellido...'
-          />
-          {errors.lastName && <span>{errors.lastName.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='dni'>Dni</label>
-          <input type='text' {...register(
-            'dni',
-            { required: 'El dni es requerido' })}
-            placeholder='Ingrese dni...'
-          />
-          {errors.dni && <span>{errors.dni.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='healthPlan'>Obra social</label>
-          <input {...register(
-              'healthPlan',
-            )}
-              placeholder='Ingrese su obra social...'
-          />
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='phone'>Teléfono</label>
-          <input {...register(
-              'phone',
-              { required: 'El número de teléfono es requerido' })}
-              placeholder='Ingrese número de teléfono...'
-          />
-          {errors.phone && <span>{errors.phone.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='email'>Correo</label>
-          <input {...register(
-            'email',
-            { required: 'El email es requerido' })}
-            placeholder='Ingrese correo...'
-          />
-        {errors.email && <span>{errors.email.message}</span>}
-        </div>
-        <div className={styles.inputBlock}>
-          <label htmlFor='password'>Contraseña</label>
-          <input type='password' {...register(
-            'password',
-            { required: 'La contraseña es requerida' })}
-            placeholder='Ingrese contraseña...'
-          />
-        {errors.password && <span>{errors.password.message}</span>}
-        </div>
-        <button
-          className={styles.submit}
-          type='submit'
+      <div className={styles.formContainer}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Enviar formulario
-        </button>
-      </form>
+          <h1 className={styles.title}>
+            Registrarse
+          </h1>
+          <div className={styles.inputBlock}>
+            <label htmlFor='firstName'>Nombre</label>
+            <input {...register('firstName')}
+                placeholder='Ingrese nombre...'
+            />
+            {errors.firstName && <span>{errors.firstName.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='lastName'>Apellido</label>
+            <input {...register(
+                'lastName',
+                { required: 'El apellido es requerido' })}
+                placeholder='Ingrese apellido...'
+            />
+            {errors.lastName && <span>{errors.lastName.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='dni'>Dni</label>
+            <input type='text' {...register(
+              'dni',
+              { required: 'El dni es requerido' })}
+              placeholder='Ingrese dni...'
+            />
+            {errors.dni && <span>{errors.dni.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='healthPlan'>Obra social</label>
+            <input {...register(
+                'healthPlan',
+              )}
+                placeholder='Ingrese su obra social...'
+            />
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='phone'>Teléfono</label>
+            <input {...register(
+                'phone',
+                { required: 'El número de teléfono es requerido' })}
+                placeholder='Ingrese número de teléfono...'
+            />
+            {errors.phone && <span>{errors.phone.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='email'>Correo</label>
+            <input {...register(
+              'email',
+              { required: 'El email es requerido' })}
+              placeholder='Ingrese correo...'
+            />
+          {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' {...register(
+              'password',
+              { required: 'La contraseña es requerida' })}
+              placeholder='Ingrese contraseña...'
+            />
+          {errors.password && <span>{errors.password.message}</span>}
+          </div>
+          <button
+            className={styles.submit}
+            type='submit'
+          >
+            Enviar formulario
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
